48eebd3d9707c280b367f747b9befe51959162e3
Safal Rana <123141574+SafalRana01@users.noreply.github.com>
2023-07-17 13:56:25 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let menu = document.querySelector('#menu-bar')
+let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let header = document.querySelector('.header-2');
 
